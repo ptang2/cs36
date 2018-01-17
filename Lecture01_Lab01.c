493c1e5d9ece50faf8a25d13edbd7a4556234021
Peter Tang <ptang2@saddleback.edu>
2018-01-16 21:34:36 -0800

    lab 1 done

---
+++ Lecture01_Lab01.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/*******************************************
+/* Lecture 1
+/*
+/* This program print the sum of three numbers
+/*
+/* Lab01
+/*
+/* Peter Tang
+/* 959213
+/* 1/16/18
+*******************************************/
+
+int main()
+{
+    int value1, value2, sum;
+
+    value1 = 10;
+    value2 = 20;
+
+    sum = value1 + value2;
+
+    printf("The sum is %d\n", sum);
+
+    return 0;
+}
+
+/*
+The sum is 30
+
+Process returned 0 (0x0)   execution time : -0.000 s
+Press any key to continue.
+*/
---
+++ Lecture01_Lab02.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+/*******************************************
+/* Lecture 1
+/*
+/* This program print the sum
+/* and average of three numbers
+/*
+/* Lab02
+/*
+/* Peter Tang
+/* 959213
+/* 1/16/18
+*******************************************/
+
+int main()
+{
+	//declaration
+    int a, b, c;
+	int sum;
+	float avg;
+
+	//data
+    a = 10;
+    b = 20;
+	c = 30;
+
+	//calculation
+    sum = a + b + c;
+	avg = sum / 3;
+
+	//output
+	printf("The 3 numbers are %d %d %d\n", a, b, c);
+    printf("The sum is %d\n", sum);
+	printf("The average is %0.2f\n", avg);
+
+    return 0;
+}
+
+/*
+The 3 numbers are 10 20 30
+The sum is 60
+The average is 20.00
+
+Process returned 0 (0x0)   execution time : 0.039 s
+Press any key to continue.
+*/
