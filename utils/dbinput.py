911d1b6b89b15dc2a41ae63360bcd98a70ff0685
Peter Tang <ptang2@saddleback.edu>
2018-02-27 21:52:25 -0800

    hw2

--- utils/dbinput.py
+++ utils/dbinput.py
@@ -8,17 +8,16 @@ if __name__ == "__main__":
         translist = ['AIR', 'TRAIN', 'BUS']
         count = 0
         for line in fin:
-            fout.write("costDB[{}][{}][{}] = {}".format(sourcelist[si], destlist[di], translist[ti], line.strip())
+            fout.write("costDB[{}][{}][{}] = {}\n".format(sourcelist[si], destlist[di], translist[ti], line.strip())
             count += 1
             if(count >11)
-                    si += 1
+                si += 1
             if(count != 0 && count % 4 == 0)
-                    di += 1
+                di += 1
             else
-                    di = 0
+                di = 0
             if(count != 0 && count % 3 == 0)
-                    ti += 1
+                ti += 1
             else
-                    ti = 0
-				
+                ti = 0
     fout.close
